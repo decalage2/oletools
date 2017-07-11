6a7e74cc95a73c907d31b307e628bbeb3b30f4d0
decalage2 <decalage@laposte.net>
2017-07-11 21:52:03 +0200

    setup.py: version 0.51.1dev1

--- setup.py
+++ setup.py
@@ -41,11 +41,11 @@ import os, fnmatch
 #--- METADATA -----------------------------------------------------------------
 
 name         = "oletools"
-version      = '0.51'
+version      = '0.51.1dev1'
 desc         = "Python tools to analyze security characteristics of MS Office and OLE files (also called Structured Storage, Compound File Binary Format or Compound Document File Format), for Malware Analysis and Incident Response #DFIR"
 long_desc    = open('oletools/README.rst').read()
 author       = "Philippe Lagadec"
-author_email = "decalage at laposte dot net"
+author_email = "nospam@decalage.info"
 url          = "http://www.decalage.info/python/oletools"
 license      = "BSD"
 download_url = "https://github.com/decalage2/oletools/releases"
