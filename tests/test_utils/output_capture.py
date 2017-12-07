1fb981e1091977aad204ac14059ffcdfea0cd8ba
Christian Herdtweck <christian.herdtweck@intra2net.com>
2017-12-07 10:57:10 +0100

    unittest: ensure that the OutputCapturer closes its buffer
    Looking for reason for ResourceWarning abount unclosed BufferedReader, but
    this was not the cause...

--- tests/test_utils/output_capture.py
+++ tests/test_utils/output_capture.py
@@ -22,13 +22,14 @@ class OutputCapture:
         for line in capturer:
             some_test(line)
         # ...or test all output in one go
-        some_test(capturer.buffer.getvalue())
+        some_test(capturer.get_data())
 
     """
 
     def __init__(self):
         self.buffer = StringIO()
         self.orig_stdout = None
+        self.data = None
 
     def __enter__(self):
         # replace sys.stdout with own buffer.
@@ -38,13 +39,25 @@ class OutputCapture:
 
     def __exit__(self, exc_type, exc_value, traceback):
         sys.stdout = self.orig_stdout    # re-set to original
+        self.data = self.buffer.getvalue()
+        self.buffer.close()              # close buffer
+        self.buffer = None
 
         if exc_type:   # there has been an error
             print('Got error during output capture!')
             print('Print captured output and re-raise:')
-            for line in self.buffer.getvalue().splitlines():
+            for line in self.data.splitlines():
                 print(line.rstrip())  # print output before re-raising
 
+    def get_data(self):
+        """ retrieve all the captured data """
+        if self.buffer is not None:
+            return self.buffer.getvalue()
+        elif self.data is not None:
+            return self.data
+        else:   # should not be possible
+            raise RuntimeError('programming error or someone messed with data!')
+
     def __iter__(self):
-        for line in self.buffer.getvalue().splitlines():
-            yield line.rstrip()   # remove newline at end of line
+        for line in self.get_data().splitlines():
+            yield line
