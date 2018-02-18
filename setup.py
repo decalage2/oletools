6b5dd1933038bfe6bd525873a40cc8ff27333747
decalage2 <decalage@laposte.net>
2018-02-18 23:02:29 +0100

    setup: added a few tags for PyPI

--- setup.py
+++ setup.py
@@ -63,7 +63,11 @@ classifiers=[
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
     "Topic :: Security",
     "Topic :: Software Development :: Libraries :: Python Modules",
 ]
