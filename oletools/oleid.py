070422457499538a7c29dee739cac0171aa6077e
decalage2 <decalage@laposte.net>
2018-04-25 06:14:25 +0200

    oleid: bumped version to 0.53dev6

--- oletools/oleid.py
+++ oletools/oleid.py
@@ -18,7 +18,7 @@ http://www.decalage.info/python/oletools
 
 #=== LICENSE =================================================================
 
-# oleid is copyright (c) 2012-2017, Philippe Lagadec (http://www.decalage.info)
+# oleid is copyright (c) 2012-2018, Philippe Lagadec (http://www.decalage.info)
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
@@ -56,7 +56,7 @@ from __future__ import print_function
 # 2017-04-26       PL: - fixed absolute imports (issue #141)
 # 2017-09-01       SA: - detect OpenXML encryption
 
-__version__ = '0.51'
+__version__ = '0.53dev6'
 
 
 #------------------------------------------------------------------------------
