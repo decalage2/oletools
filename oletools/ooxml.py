d796314c7a98f219b700433ae1fe911674628011
Christian Herdtweck <christian.herdtweck@intra2net.com>
2018-11-02 17:19:49 +0100

    ooxml: open files in binary mode
    The xml parser takes the encoding from the file header

--- oletools/ooxml.py
+++ oletools/ooxml.py
@@ -436,7 +436,7 @@ class XmlParser(object):
             if args:
                 raise BadOOXML(self.filename, 'xml has no subfiles')
             # do not use uopen, xml parser determines encoding on its own
-            with open(self.filename, 'r') as handle:
+            with open(self.filename, 'rb') as handle:
                 yield None, handle   # the subfile=None is needed in iter_xml
             self.did_iter_all = True
         else:
