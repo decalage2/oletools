5bf585f5512a0f59788df85dfd33f84e53e689af
Christian Herdtweck <christian.herdtweck@intra2net.com>
2018-12-06 15:26:59 +0100

    ooxml: Create __version__, add license and start changelog

--- oletools/ooxml.py
+++ oletools/ooxml.py
@@ -13,7 +13,39 @@ TODO: check what is duplicate here with oleid, maybe merge some day?
 TODO: "xml2003" == "flatopc"?
 
 .. codeauthor:: Intra2net AG <info@intra2net>
+License: BSD, see source code or documentation
+
+msodde is part of the python-oletools package:
+http://www.decalage.info/python/oletools
 """
+
+# === LICENSE =================================================================
+
+# msodde is copyright (c) 2017-2019 Philippe Lagadec (http://www.decalage.info)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# -- IMPORTS ------------------------------------------------------------------
 
 import sys
 from oletools.common.log_helper import log_helper
@@ -29,6 +61,18 @@ try:
     import lxml.etree as ET
 except ImportError:
     import xml.etree.cElementTree as ET
+
+# -----------------------------------------------------------------------------
+# CHANGELOG:
+# 2018-12-06       CH: - ensure stdout can handle unicode
+
+__version__ = '0.54.2'
+
+
+###############################################################################
+# CONSTANTS
+###############################################################################
+
 
 logger = log_helper.get_or_create_silent_logger('ooxml')
 
