76ca721915fecb1fd8771b8d9ef3b6d6f4d3d520
decalage2 <decalage@laposte.net>
2019-03-20 22:20:45 +0100

    olevba: enabled XLM/XLF macros detection for Python 3

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ import os, fnmatch
 #--- METADATA -----------------------------------------------------------------
 
 name         = "oletools"
-version      = '0.54dev10'
+version      = '0.54dev11'
 desc         = "Python tools to analyze security characteristics of MS Office and OLE files (also called Structured Storage, Compound File Binary Format or Compound Document File Format), for Malware Analysis and Incident Response #DFIR"
 long_desc    = open('oletools/README.rst').read()
 author       = "Philippe Lagadec"
