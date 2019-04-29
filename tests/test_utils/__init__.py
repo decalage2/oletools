1e0c967e2d62bfabbddded19aafa79bc2998157b
Christian Herdtweck <christian.herdtweck@intra2net.com>
2019-04-29 13:12:41 +0200

    tests: Move constants into proper module

--- tests/test_utils/__init__.py
+++ tests/test_utils/__init__.py
@@ -1,10 +1,1 @@
-from os.path import dirname, join, abspath
-
-# Base dir of project, contains subdirs "tests" and "oletools" and README.md
-PROJECT_ROOT = dirname(dirname(dirname(abspath(__file__))))
-
-# Directory with test data, independent of current working directory
-DATA_BASE_DIR = join(PROJECT_ROOT, 'tests', 'test-data')
-
-# Directory with source code
-SOURCE_BASE_DIR = join(PROJECT_ROOT, 'oletools')
+from .utils import *
