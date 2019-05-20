69d534babfa0f5ad44d50024a593ac7704c05bc1
decalage2 <decalage@laposte.net>
2019-05-20 09:06:55 +0200

    bumped version to 0.54.2

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ import os, fnmatch
 #--- METADATA -----------------------------------------------------------------
 
 name         = "oletools"
-version      = '0.54.1'
+version      = '0.54.2'
 desc         = "Python tools to analyze security characteristics of MS Office and OLE files (also called Structured Storage, Compound File Binary Format or Compound Document File Format), for Malware Analysis and Incident Response #DFIR"
 long_desc    = open('oletools/README.rst').read()
 author       = "Philippe Lagadec"
