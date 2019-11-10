c46fc950918ab8597795b10312100fcd88e7c9a2
decalage2 <decalage@laposte.net>
2019-11-10 21:53:01 +0100

    setup.py, requirements.txt: changed pyparsing from >=2.2.0 to >=2.1.0, fixes #481

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ to install this package.
 # 2019-05-22       PL: - 'msoffcrypto-tool' is now a required dependency
 # 2019-05-23 v0.55 PL: - added pcodedmp as dependency
 # 2019-09-24       PL: - removed oletools.thirdparty.DridexUrlDecoder
+# 2019-11-10       PL: - changed pyparsing from 2.2.0 to 2.1.0 for issue #481
 
 #--- TODO ---------------------------------------------------------------------
 
@@ -51,7 +52,7 @@ import os, fnmatch
 #--- METADATA -----------------------------------------------------------------
 
 name         = "oletools"
-version      = '0.55.dev4'
+version      = '0.55.dev5'
 desc         = "Python tools to analyze security characteristics of MS Office and OLE files (also called Structured Storage, Compound File Binary Format or Compound Document File Format), for Malware Analysis and Incident Response #DFIR"
 long_desc    = open('oletools/README.rst').read()
 author       = "Philippe Lagadec"
@@ -315,7 +316,7 @@ def main():
         test_suite="tests",
         # scripts=scripts,
         install_requires=[
-            "pyparsing>=2.2.0",
+            "pyparsing>=2.1.0",  # changed from 2.2.0 to 2.1.0 for issue #481
             "olefile>=0.46",
             "easygui",
             'colorclass',
