b284c6c52ef1b991337086dcb3d337999891d204
decalage2 <decalage@laposte.net>
2019-12-03 20:30:16 +0100

    crypto: replaced log.warning by log.info for error messages that trigger on all non OLE files

--- oletools/crypto.py
+++ oletools/crypto.py
@@ -219,7 +219,8 @@ def is_encrypted(some_file):
             return msoffcrypto.OfficeFile(file_handle).is_encrypted()
 
         except Exception as exc:
-            log.warning('msoffcrypto failed to interpret file {} or determine '
+            # TODO: this triggers unnecessary warnings for non OLE files
+            log.info('msoffcrypto failed to interpret file {} or determine '
                         'whether it is encrypted: {}'
                         .format(file_handle.name, exc))
 
@@ -242,7 +243,8 @@ def is_encrypted(some_file):
         with OleFileIO(some_file) as ole:
             return _is_encrypted_ole(ole)
     except Exception as exc:
-        log.warning('Failed to check {} for encryption ({}); assume it is not '
+        # TODO: this triggers unnecessary warnings for non OLE files
+        log.info('Failed to check {} for encryption ({}); assume it is not '
                     'encrypted.'.format(some_file, exc))
 
     return False
