4de7d5ff7beb08ea9287f7bfda3c621567e1bf6a
Christian Herdtweck <christian.herdtweck@intra2net.com>
2022-06-24 11:31:16 +0200

    log_helper: Test and improve error-handling
    If there is an error in log-handling (e.g. wrong args to logger call) do
    not fail or raise but produce something helpful

--- oletools/common/log_helper/_json_formatter.py
+++ oletools/common/log_helper/_json_formatter.py
@@ -32,15 +32,25 @@ class JsonFormatter(logging.Formatter):
         :py:class:`oletools.common.log_helper.OletoolsLoggerAdapter` or added here
         (for input from e.g. captured warnings, third-party libraries)
         """
-        msg = self.msg_formatter.format(record)
-        json_dict = dict(msg=msg.replace('\n', ' '), level=record.levelname)
+        json_dict = dict(msg='', level='', type='')
         try:
+            msg = self.msg_formatter.format(record)
+            json_dict['msg'] = msg.replace('\n', ' ')
+            json_dict['level'] = record.levelname
             json_dict['type'] = record.type
-        except AttributeError:
-            if record.name == 'py.warnings':   # this is the name of the logger
+        except AttributeError:     # most probably: record has no "type" field
+            if record.name == 'py.warnings':   # this is from python's warning-capture logger
                 json_dict['type'] = 'warning'
             else:
-                json_dict['type'] = 'msg'
+                json_dict['type'] = 'msg'      # message of unknown origin
+        except Exception as exc:
+            try:
+                json_dict['msg'] = "Ignore {0} when formatting '{1}': {2}".format(type(exc), record.msg, exc)
+            except Exception as exc2:
+                json_dict['msg'] = 'Caught {0} in logging'.format(str(exc2))
+            json_dict['type'] = 'log-warning'
+            json_dict['level'] = 'warning'
+
         formatted_message = '    ' + json.dumps(json_dict)
 
         if self._is_first_line:
